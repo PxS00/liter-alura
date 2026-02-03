efe10b9a861203dcba2a05e314b992cf55afc09a
PxS00 <lucas.rossonidd@gmail.com>
2026-02-03 11:41:31 -0300

    feat(model): add Author class and update Book class for author management

---
+++ src/main/java/br/com/literalura/model/Author.java
@@ -0,0 +1,57 @@
+package br.com.literalura.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer birthYear;
+    private Integer deathYear;
+    @ManyToOne
+    private Book books;
+
+    public Author(AuthorData authorData) {
+        this.name = authorData.name();
+        this.birthYear = authorData.birthYear();
+        this.deathYear = authorData.deathYear();
+    }
+
+    public Author() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public Integer getDeathYear() {
+        return deathYear;
+    }
+
+    public void setDeathYear(Integer deathYear) {
+        this.deathYear = deathYear;
+    }
+
+    public Book getBooks() {
+        return books;
+    }
+
+    public void setBooks(Book books) {
+        this.books = books;
+    }
+}
--- src/main/java/br/com/literalura/model/Book.java
+++ src/main/java/br/com/literalura/model/Book.java
@@ -1,10 +1,19 @@
 package br.com.literalura.model;
 
+import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
     private String title;
+    @OneToMany(mappedBy = "books", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Author> author = new ArrayList<>();
     private String language;
     private Integer downloads;
@@ -14,6 +23,7 @@ public class Book {
         this.author = bookData.author().stream()
                 .map(Author::new)
                 .toList();
+        this.author.forEach(a -> a.setBooks(this));
         this.language = bookData.language().isEmpty() ? "Unknown" : bookData.language().get(0);
         this.downloads = bookData.downloads();
     }
@@ -34,7 +44,7 @@ public class Book {
     }
 
     public void setAuthor(List<Author> author) {
-        author.forEach(a -> a.setBook(this));
+        author.forEach(a -> a.setBooks(this));
         this.author = author;
     }
 
