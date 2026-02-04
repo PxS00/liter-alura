a549f9518f4dd7f2a23bffa57c46d8a6ab9dd2d8
PxS00 <lucas.rossonidd@gmail.com>
2026-02-04 18:04:17 -0300

    fix(api): enhance HttpClient configuration and improve error handling

--- src/main/java/br/com/literalura/service/ApiClient.java
+++ src/main/java/br/com/literalura/service/ApiClient.java
@@ -7,22 +7,35 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ApiClient {
-    private static final HttpClient client = HttpClient.newHttpClient();
+
+    private static final HttpClient client =
+            HttpClient.newBuilder()
+                    .followRedirects(HttpClient.Redirect.ALWAYS)
+                    .build();
 
     public String getData(String url) {
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .GET()
                 .build();
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            if (response.statusCode() != 200) {
-                throw new RuntimeException("Failed to fetch data: HTTP status code " + response.statusCode());
+        try {
+
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() < 200 || response.statusCode() >= 300) {
+                throw new RuntimeException(
+                        "Failed to fetch data: HTTP status code " + response.statusCode()
+                );
             }
+
             return response.body();
+
         } catch (IOException e) {
-            throw new RuntimeException("I/O error while consuming the API.",e);
+            throw new RuntimeException("I/O error while consuming the API.", e);
+
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException("Thread interrupted", e);
