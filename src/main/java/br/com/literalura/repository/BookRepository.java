f82ca55509938d950718e61710d166adf60498a2
PxS00 <lucas.rossonidd@gmail.com>
2026-02-04 12:19:49 -0300

    feat(repository): add methods to find books by title and authors by living year

--- src/main/java/br/com/literalura/repository/BookRepository.java
+++ src/main/java/br/com/literalura/repository/BookRepository.java
@@ -1,7 +1,16 @@
 package br.com.literalura.repository;
 
+import br.com.literalura.model.Author;
 import br.com.literalura.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByTitleContainingIgnoreCase(String bookTitle);
+
+    @Query("SELECT a FROM Author a WHERE a.birthYear <= :year AND (a.deathYear IS NULL OR a.deathYear >= :year)")
+    List<Author> authorsByLivingYear(Integer year);
 }
